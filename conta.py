0d25d37a0054b25b3792986e3c7282ddd0a35068
Fernanda Ishida <fer.ishida@gmail.com>
2023-05-31 23:14:13 -0300

    Getters e Setters

--- conta.py
+++ conta.py
@@ -5,7 +5,7 @@ class Conta:
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
-        self.__limte = limite
+        self.__limite = limite
 
     def extrato(self):
         print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
@@ -20,3 +20,14 @@ class Conta:
         self.saca(valor)
         destino.deposita(valor)
 
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, limite):
+        self.__limite = limite
