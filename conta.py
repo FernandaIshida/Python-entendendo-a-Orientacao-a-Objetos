62c695b5658bfb464038c42e8a317b7db712f9a6
Fernanda Ishida <fer.ishida@gmail.com>
2023-05-31 21:15:36 -0300

    Usando metodos

--- conta.py
+++ conta.py
@@ -7,3 +7,13 @@ class Conta:
         self.saldo = saldo
         self.limte = limite
 
+    def extrato(self):
+        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
+
+
